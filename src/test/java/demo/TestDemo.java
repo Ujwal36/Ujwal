3c71fa8a66c666ea2e243b779d50ac69f3356e32
Ujwal36 <35258482+Ujwal36@users.noreply.github.com>
2026-06-22 22:00:57 +0530

    Update TestDemo.java

--- src/test/java/demo/TestDemo.java
+++ src/test/java/demo/TestDemo.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
